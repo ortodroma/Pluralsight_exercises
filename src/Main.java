f886cc7bbf73b71d2abb2aaf703934ace836d779
AMaticic <ana.maticic@protonmail.com>
2022-01-19 10:12:52 +0100

    Count the number of unique hashtags

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,38 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         BufferedReader bufferedReader = null;
         String filename = "/Users/AMaticic/OneDrive - ENDAVA/Desktop/TweetData.txt";
-                String line = "";
+        String line = "";
+        List<String> hashtags = new ArrayList<String>();
 
         try {
             bufferedReader = new BufferedReader(new FileReader(filename));
 
-            while ((line=bufferedReader.readLine()) != null) {
-System.out.println("tweet = " + linev);
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] words = line.split(" ");
+                for (String word : words) {
+                    if (word.contains("#")) {
+                       word=word.replaceAll("^[^#]*", "");
+                       if (!hashtags.contains(word))
+                        hashtags.add(word);
+                    }
+                }
             }
+            hashtags.sort(String::compareTo);
+            for (String hashtag : hashtags) {
+                System.out.println(hashtag);
+            }
+
+            System.out.println("Count = " + hashtags.size());
+
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
